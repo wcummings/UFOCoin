ef481bd8f7a8f120d78f9ce3d7e8982be738decd
William Cummings <will@wpc.io>
2017-11-28 17:22:49 -0500

    Timeout on :gen_tcp.accept/2

--- lib/mbc/p2p/acceptor.ex
+++ lib/mbc/p2p/acceptor.ex
@@ -2,16 +2,22 @@ alias MBC.P2P.HandshakeSupervisor, as: HandshakeSupervisor
 
 defmodule MBC.P2P.Acceptor do
 
+  @accept_timeout 1000
+  
   def start_link(port) do
     {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: 4, active: false, reuseaddr: true])    
     {:ok, spawn_link fn -> loop(socket) end}
   end
 
   def loop(socket) do
-    {:ok, client} = :gen_tcp.accept(socket)
-    {:ok, pid} = HandshakeSupervisor.start_child(client)
-    :ok = :gen_tcp.controlling_process(client, pid)
-    loop(socket)
+    case :gen_tcp.accept(socket, @accept_timeout) do
+      {:ok, client} -> 
+	{:ok, pid} = HandshakeSupervisor.start_child(client)
+	:ok = :gen_tcp.controlling_process(client, pid)
+	loop(socket)
+      {:error, :timeout} ->
+	loop(socket)
+    end
   end
   
 end
