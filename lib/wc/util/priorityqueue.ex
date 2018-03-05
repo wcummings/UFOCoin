3ab326120ad38856f9a381539c31209ca880fb93
William Cummings <will@wpc.io>
2018-03-05 15:51:44 -0500

    Use set to check queue membership

--- lib/wc/util/priorityqueue.ex
+++ lib/wc/util/priorityqueue.ex
@@ -1,41 +1,40 @@
-# TODO: implement max size, this thing is not gunna deal w/ a lot of elements well
+# TODO: implement max size, this thing is horribly inefficient
 
 defmodule WC.Util.PriorityQueue do
   @moduledoc "Simple priority queue implementation w/ integer ranks."
-  @defstruct [:map]
+  @defstruct [:map, :set]
 
   @opaque t :: %WC.Util.PriorityQueue{}
 
   def init do
-    %WC.Util.PriorityQueue{}
+    %WC.Util.PriorityQueue{map: %{}, set: MapSet.new}
   end
 
   @spec insert(t, integer, term()) :: t
   def insert(pq, rank, item) do
-    %{pq | map: Map.update(pq.map, get_next_available_key(rank), item)}
+    %{pq | map: Map.update(pq.map, get_next_available_key(rank), item), set: MapSet.put(pq.set, item)}
   end
 
   @spec member?(t, term()) :: true | false
   def member?(pq, item) do
-    Enum.member?(Map.values(pq.map), item)
+    MapSet.member?(pq.set, item)
   end
 
-  @spec delete(t, term()) :: t
-  def delete(pq, item) do
-    key = Map.keys(pq.map) |> Enum.find(fn key -> pq.map[key] == item end)
-    if key != nil do
-      %{pq | map: Map.delete(pq.map, key)}
+  @spec delete(term(), t) :: t
+  def delete(item, pq) do
+    if member?(pq, item) do
+      %{pq | map: Map.delete(pq.map, key), set: MapSet.delete(pq.set, item)}
     else
       pq
     end
   end
 
-  @spec drain(t) :: {t, list()}
-  def drain(pq) do
-    items = Map.keys(pq.map)
+  @spec get(t, integer) :: list()
+  def get(pq, max_rank) do
+    Map.keys(pq.map)
     |> Enum.sort
+    |> Enum.filter(fn -> key < max_rank end)
     |> Enum.map(fn key -> pq.map[key] end)
-    {%{pq | map: %{}}, items}
   end
 
   def get_next_available_key(key, map) do
