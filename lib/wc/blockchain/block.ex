3a72715c373a750ea93d4f42a03e3acf6f36c508
William Cummings <will@wpc.io>
2018-03-15 12:15:01 -0400

    shorthand

--- lib/wc/blockchain/block.ex
+++ lib/wc/blockchain/block.ex
@@ -10,10 +10,10 @@ defmodule WC.Blockchain.Block do
 
   @type block_validation_error :: :notfound | :orphan | :badheight | :alreadyaccepted | :baddifficulty
   @type encoded_block :: binary
-  @type t :: %WC.Blockchain.Block{header: BlockHeader.t, txs: list(TX.t)}
+  @type t :: %__MODULE__{header: BlockHeader.t, txs: list(TX.t)}
 
   @spec encode(t) :: encoded_block
-  def encode(%WC.Blockchain.Block{header: block_header, txs: _txs}) do
+  def encode(%__MODULE__{header: block_header, txs: _txs}) do
     BlockHeader.encode(block_header) # FIXME: Ignoring TX's for now
   end
 
@@ -22,11 +22,11 @@ defmodule WC.Blockchain.Block do
   @spec decode(encoded_block) :: t
   def decode(encoded_block_header) do
     block_header = BlockHeader.decode(encoded_block_header)
-    %WC.Blockchain.Block{header: block_header, txs: []} # FIXME: Ignoring TX's for now
+    %__MODULE__{header: block_header, txs: []} # FIXME: Ignoring TX's for now
   end
 
   @spec hash(t) :: BlockHeader.block_hash
-  def hash(%WC.Blockchain.Block{header: block_header}) do
+  def hash(%__MODULE__{header: block_header}) do
     BlockHeader.hash(block_header)
   end
 
@@ -36,13 +36,13 @@ defmodule WC.Blockchain.Block do
   end
 
   @spec next_block(t) :: t
-  def next_block(prev_block = %WC.Blockchain.Block{header: prev_block_header, txs: _txs}) do
+  def next_block(prev_block = %__MODULE__{header: prev_block_header, txs: _txs}) do
     prev_block_hash = BlockHeader.hash(prev_block_header)
     block_header = %BlockHeader{prev_block_hash: prev_block_hash,
 				height: prev_block_header.height + 1,
 				difficulty: get_current_difficulty(prev_block),
 				timestamp: :os.system_time(:millisecond)}
-    %WC.Blockchain.Block{header: block_header, txs: []}
+    %__MODULE__{header: block_header, txs: []}
   end
 
   @spec equal?(t, t) :: true | false
@@ -54,7 +54,7 @@ defmodule WC.Blockchain.Block do
   # the caller should be able to trust that an orphaned block is valid,
   # aside from being an orphan.
   @spec validate(t) :: :ok | {:error, block_validation_error}
-  def validate(new_block = %WC.Blockchain.Block{header: block_header}) do
+  def validate(new_block = %__MODULE__{header: block_header}) do
     case BlockHeader.check_nonce(block_header) do
       {true, _} ->
 	check_prev_block(new_block)
@@ -63,7 +63,7 @@ defmodule WC.Blockchain.Block do
     end
   end
 
-  def check_prev_block(block = %WC.Blockchain.Block{header: %BlockHeader{prev_block_hash: prev_block_hash}}) do
+  def check_prev_block(block = %__MODULE__{header: %BlockHeader{prev_block_hash: prev_block_hash}}) do
     case LogServer.get_block_by_hash(prev_block_hash) do
       {:error, :notfound} ->
 	{:error, :orphan}
